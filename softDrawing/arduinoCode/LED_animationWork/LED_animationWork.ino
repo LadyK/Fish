b5871ff98cf803d034f49a993aad440e0a5753b3
LadyK <LadyK@users.noreply.github.com>
2022-01-03 20:55:36 -0500

    control down; needs more testing; working on controlling many structures

--- softDrawing/arduinoCode/LED_animationWork/LED_animationWork.ino
+++ softDrawing/arduinoCode/LED_animationWork/LED_animationWork.ino
@@ -8,7 +8,7 @@
 #define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 3
+#define LED_COUNT 24
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -39,6 +39,16 @@ int startAdd = 0; // LED start address
 // load structures into array:
 //Structure structures[8] = {one, two, three, four, five, six, seven, eight};
 //Structure structures[1] = {one};
+int homeAddress[8] = {0, 3, 6, 9, 12, 15, 18, 21};
+
+int rl[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+int mbf[8] = {1, 5, 2, 3, 6, 0, 4, 7};
+int fb[8] = {4, 7, 0, 5, 1, 3, 6, 2};
+int bf[8] = {3, 2, 6, 1, 5, 0, 4, 7};
+int bf2[8] = {6, 3, 5, 2, 1, 7, 4, 0};
+int fb2[8] = {0, 1, 4, 5, 7, 3, 2, 6};
+
+String seqs[8] = {"rl", "mbf", "fb", "bf", "bf2", "fb2", "r", "r"};
 
 boolean newStructure;  //
 long pixelHue; //
@@ -184,14 +194,16 @@ void loop() {
   // ******** change color *********
   //
   //  //change color:
-  if ( liteAny == true) {  //change this variable to be associated with any of them lite
+  //if ( liteAny == true) {  //change this variable to be associated with any of them lite
+
+  if (pixelHue > 5 * 65536) {
+    pixelHue = 0;
+  } else {
     pixelHue += 256;
-    if (pixelHue > 5 * 65536) {
-      pixelHue = 0;
-    }
-    Serial.print("pixelHue is: ");
-    Serial.println(pixelHue);
   }
+  Serial.print("pixelHue is: ");
+  Serial.println(pixelHue);
+  //}
 
   //  for (long firstPixelHue = pixelHue; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
   //  }
@@ -208,9 +220,10 @@ void loop() {
     birthTime = millis();
     lifeSpan = birthTime + lifeSpan; // update lifeSpan with relation to currentDate
     someoneNear = false;
-   // fade = false;
+    // fade = false;
     offCount = 0;
-    Serial.println("            can't repeat until new trigger");
+    //Serial.println("            can't repeat until new trigger");
+    animationSelection();
   } // someoneNear
 
 
@@ -224,182 +237,230 @@ void loop() {
       fade = true;
       Serial.println("        fading");
     } else { // regular lighting:   //if (fade == false)
-      //for (int i = startAddress; i < howManyStructures; i+=3) {
-      pixelHue = light(pixelHue, startAddress); // eventually make startAddress  = i
-      Serial.println("        Just lighting");
+
+      // startAddress,  delay:
+      //for (int i = startAddress; i < sizeof(homeAddress) / sizeof(int); i += 3) {
+      int count = 0;
+      while (count < sizeof(homeAddress) / sizeof(int) ) {
+
+        // Serial.print("pixelHue is: ");
+        // Serial.println(pixelHue);
+        pixelHue = light(pixelHue, homeAddress[startAddress]); // eventually make startAddress  = i
+        Serial.println("        Just lighting");
+        count++;
+        delay(3000);
+      }
+
+
     }
-  }
-  else if ( someoneNear == false && liteAny == true && lifeSpan < millis() ) { // flip off
-    liteAny = false;
-    Serial.println("        off");
-  }
+    else if ( someoneNear == false && liteAny == true && lifeSpan < millis() ) { // flip off
+      liteAny = false;
+      lifeSpan = 0;  // reset here ?
+      Serial.println("        off");
+    }
 
 
-  if ( (fade == false) && liteAny == true) {
-    
-  }
+    if ( (fade == false) && liteAny == true) {
 
-  else if (fade == true && liteAny == true) {
-    fadeDown(pixelHue, startAddress);
-    fade = false;
-  } else if (liteAny == false) {
-    turnOff(startAddress, 50);
-    offCount++;
-  }
 
-  //  // run, fade or die:
-  //  if (temp.almostUp == false && temp.lite == true) {
-  //    Serial.println("      lighting");
-  //    pixelHue = temp.light(pixelHue);  // <--- need to return color as it's changing a bit in the class
-  //  } else if (temp.almostUp == true && temp.lite == true) {
-  //    // Serial.println("      fading");
-  //    // temp.fadeDown(pixelHue);
-  //  } else if (temp.almostUp == true && temp.lite == false) {
-  //    // Serial.println("      turnOff");
-  //    //structures[i].lite = false;
-  //    // temp.turnOff(0, 30);
-  //    offCount++;
-  //  }
+    }
 
-  // bool on_off = structures[i].go(pixelHue);
+    else if (fade == true && liteAny == true) {
+      fadeDown(pixelHue, startAddress);
+      fade = false;
+    } else if (liteAny == false) {
+      turnOff(startAddress, 50);
+      //offCount++;
+    }
+
+    //  // run, fade or die:
+    //  if (temp.almostUp == false && temp.lite == true) {
+    //    Serial.println("      lighting");
+    //    pixelHue = temp.light(pixelHue);  // <--- need to return color as it's changing a bit in the class
+    //  } else if (temp.almostUp == true && temp.lite == true) {
+    //    // Serial.println("      fading");
+    //    // temp.fadeDown(pixelHue);
+    //  } else if (temp.almostUp == true && temp.lite == false) {
+    //    // Serial.println("      turnOff");
+    //    //structures[i].lite = false;
+    //    // temp.turnOff(0, 30);
+    //    offCount++;
+    //  }
+
+    // bool on_off = structures[i].go(pixelHue);
+    /*
+      Serial.print("structure went: ");
+      Serial.println(on_off);
+      Serial.println();
+    */
+    //If it's off, count it
+    //        if (on_off == false) {
+    //          offCount++;
+    //        }
+    /*
+      Serial.print("offCount: ");
+      Serial.println(offCount);
+      Serial.println();
+    */
+    //structures[i] = temp; // must put it back in/update it
+    delay(2000);
+  } // lite and young
+
+  //Serial.print("offCount is: ");
+  //Serial.println(offCount);
+
+
+  //    // each will get the same color before the color is changed:
+  //    if ( liteAny == true) {  //change this variable to be associated with any of them lite
+  //      pixelHue += 256;
+  //      if (pixelHue > 5 * 65536) {
+  //        pixelHue = 0;
+  //      }
+  //      Serial.print("pixelHue is: ");
+  //      Serial.println(pixelHue);
+  //    } else
+  //if (offCount >= (sizeof(structures) / sizeof(structures[0]))) {
+  // liteAny = false;
+  //break; // stop while loop
+  //offCount = 0;
+  //}
+  //pixelHue += 500;
+  // } // while loop
+  //   offCount = 0;
+  //  Serial.println("offCount zero'd");
+  //liteAny = false;
+
+  //} // lite = true
+
+
   /*
-    Serial.print("structure went: ");
-    Serial.println(on_off);
+    Serial.print(" Hue (while lighting) is: ");
+    Serial.println(pixelHue);
     Serial.println();
-  */
-  //If it's off, count it
-  //        if (on_off == false) {
-  //          offCount++;
-  //        }
-  /*
+
     Serial.print("offCount: ");
     Serial.println(offCount);
     Serial.println();
+
+    Serial.print("# structures: ");
+    Serial.println(sizeof(structures));
+    Serial.println();
+
+    Serial.print("liteAny is: ");
+    Serial.println(liteAny);
+    Serial.println();
   */
-  //structures[i] = temp; // must put it back in/update it
-  delay(2000);
-} // for loop
-
-//Serial.print("offCount is: ");
-//Serial.println(offCount);
-
-
-//    // each will get the same color before the color is changed:
-//    if ( liteAny == true) {  //change this variable to be associated with any of them lite
-//      pixelHue += 256;
-//      if (pixelHue > 5 * 65536) {
-//        pixelHue = 0;
-//      }
-//      Serial.print("pixelHue is: ");
-//      Serial.println(pixelHue);
-//    } else
-//if (offCount >= (sizeof(structures) / sizeof(structures[0]))) {
-// liteAny = false;
-//break; // stop while loop
-//offCount = 0;
-//}
-//pixelHue += 500;
-// } // while loop
-//   offCount = 0;
-//  Serial.println("offCount zero'd");
-//liteAny = false;
-
-//} // lite = true
-
-
-/*
-  Serial.print(" Hue (while lighting) is: ");
-  Serial.println(pixelHue);
-  Serial.println();
-
-  Serial.print("offCount: ");
-  Serial.println(offCount);
-  Serial.println();
-
-  Serial.print("# structures: ");
-  Serial.println(sizeof(structures));
-  Serial.println();
-
-  Serial.print("liteAny is: ");
-  Serial.println(liteAny);
-  Serial.println();
-*/
 
 
 
-/*
-  Serial.print("       offCount: ");
-  Serial.println(offCount);
-  Serial.println();
+  /*
+    Serial.print("       offCount: ");
+    Serial.println(offCount);
+    Serial.println();
 
-  Serial.print("       someoneNear is: ");
-  Serial.println(someoneNear);
-  Serial.print("       liteAny is: ");
-  Serial.println(liteAny);
+    Serial.print("       someoneNear is: ");
+    Serial.println(someoneNear);
+    Serial.print("       liteAny is: ");
+    Serial.println(liteAny);
 
-  delay(3000);
-  Serial.print(" Hue (while lighting) is: ");
-  Serial.println(pixelHue);
-  Serial.println();
-*/
-/*
-  Serial.print("# structures: ");
-  Serial.println(sizeof(structures) / sizeof(structures[0]));
-  Serial.println();
-*/
-
-
-// once lite, same color to each
-//  if (newStructure == false && triggered == true) {
-//  while (millis() - currentTime < lightPeriod)
-//   {
-//for (long firstPixelHue = pixelHue; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
-//        for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
-//          Serial.println("changing colors");
-//Serial.println();
-// Offset pixel hue by an amount to make one full revolution of the
-// color wheel (range of 65536) along the length of the strip
-// (strip.numPixels() steps):
-//pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-// strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
-// optionally add saturation and value (brightness) (each 0 to 255).
-// Here we're using just the single-argument hue variant. The result
-// is passed through strip.gamma32() to provide 'truer' colors
-//Serial.print("for-loop pixelHue is: ");
-// Serial.println(pixelHue);
-
-// before assigning to each pixel:
-//          strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(firstPixelHue)));
-
-//  }
-//        strip.show(); // Update strip with new contents
-//        delay(5);  // Pause for a moment
-//        if (millis() - currentTime < lightPeriod) {
-//          break;
-//          Serial.println("*****************breaking******************");
-//        } else {
-//          Serial.println();
-//          Serial.println("still in");
-//        }
-
-//  } // for-loop pixelHue
-
-//  } // while
-//    triggered = false;
-//  } //if
-//  else {
-//    colorWipe(0, 3);
-//  }
-
-//strip.fill(color, first, count); //Multiple pixels can be set to the same color using the fill() function, which accepts one to three arguments.
-
-//}// loop
-
-void howOld() {
+    delay(3000);
+    Serial.print(" Hue (while lighting) is: ");
+    Serial.println(pixelHue);
+    Serial.println();
+  */
+  /*
+    Serial.print("# structures: ");
+    Serial.println(sizeof(structures) / sizeof(structures[0]));
+    Serial.println();
+  */
 
 
+  // once lite, same color to each
+  //  if (newStructure == false && triggered == true) {
+  //  while (millis() - currentTime < lightPeriod)
+  //   {
+  //for (long firstPixelHue = pixelHue; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+  //        for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+  //          Serial.println("changing colors");
+  //Serial.println();
+  // Offset pixel hue by an amount to make one full revolution of the
+  // color wheel (range of 65536) along the length of the strip
+  // (strip.numPixels() steps):
+  //pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+  // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+  // optionally add saturation and value (brightness) (each 0 to 255).
+  // Here we're using just the single-argument hue variant. The result
+  // is passed through strip.gamma32() to provide 'truer' colors
+  //Serial.print("for-loop pixelHue is: ");
+  // Serial.println(pixelHue);
 
+  // before assigning to each pixel:
+  //          strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(firstPixelHue)));
+
+  //  }
+  //        strip.show(); // Update strip with new contents
+  //        delay(5);  // Pause for a moment
+  //        if (millis() - currentTime < lightPeriod) {
+  //          break;
+  //          Serial.println("*****************breaking******************");
+  //        } else {
+  //          Serial.println();
+  //          Serial.println("still in");
+  //        }
+
+  //  } // for-loop pixelHue
+
+  //  } // while
+  //    triggered = false;
+  //  } //if
+  //  else {
+  //    colorWipe(0, 3);
+  //  }
+
+  //strip.fill(color, first, count); //Multiple pixels can be set to the same color using the fill() function, which accepts one to three arguments.
+
+}// loop
+
+void animationSelection() { // delay, structure order
+  //int homeAddress[8] = {0, 3, 6, 9, 12, 15, 18, 21}; // put here for reference
+  String sequence;
+  int ranSeq = int(random(1, 10);
+  if (randSeq < 1) {
+  sequence = seqs[0];
+  }
+  else if (randSeq < 2) {
+  sequence = seqs[1];
+  }
+  else if (randSeq < 3) {
+  sequence = seqs[2];
+  }
+  else if (randSeq < 4) {
+  sequence = seqs[3];
+  }
+  else if (randSeq < 5) {
+  sequence = seqs[4];
+  }
+  else if (randSeq < 6) {
+  sequence = seqs[5];
+  }
+  else if (randSeq < 7) {
+  sequence = seqs[6];
+  }
+  else if (randSeq < 8) {
+  sequence = seqs[7];
+  }
+  else if (randSeq < 9) {
+  sequence = seqs[8];
+  }
+  Serial.print("seq selected is: ");
+  Serial.println(sequence);
+  
+
+
+  int randDelay = int(random(1000, 5000));
 }
+
+
 
 int readSensor() {
   /*
@@ -445,11 +506,13 @@ void fadeDown(long pH, int address) {
 
 long light(long pH, int address) {
   long c; // color to be returned
-  for (long j = pH; j < pH + 100; j += 10) { // change hue
-
+  long limit = pH + 100;
+  for (long j = pH; j < limit; j += 500) { // change hue
     for (int i = address; i < address + 3; i++) { // hit each pixel in a structure
       strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(j, 255, 255)));
     }
+    // Serial.print("light() color is: ");
+    // Serial.println(j);
     strip.show();
     delay(250);
     c = j;
